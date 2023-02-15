1ec65fa60b3ab1e5d0649b12c3566bdaf478666a
Yugandhar Patil <yugandhar.patil@learner.manipal.edu>
2023-02-16 00:48:40 +0530

    Hello!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,32 @@
 const Footer = () => {
 	return (
 		<footer>
-			<div className="row">
-				<div className="col col1">
-					<h3 className="text-left pb-3 ps-4">Connect with Us!</h3>
-					<div className="social-icons pt-2">
+			<div className="hii">
+				<img src={`${process.env.PUBLIC_URL}/images/grauxxx.png`} alt="" />
+				<div className="col1 text-center">
+					<h3>Connect with Us!</h3>
+					<div className="social-icons mx-auto pt-4">
 						<a href="">
-							<i class="fa-lg fa-brands fa-twitter"></i>
+							<i class="fa-xl fa-brands fa-twitter"></i>
 						</a>
 						<a href="">
-							<i class="fa-lg fa-brands fa-discord"></i>
+							<i class="fa-xl fa-brands fa-discord"></i>
 						</a>
 						<a href="">
-							<i class="fa-lg fa-brands fa-telegram"></i>
+							<i class="fa-xl fa-brands fa-telegram"></i>
 						</a>
 						<a href="">
-							<i class="fa-lg fa-brands fa-reddit"></i>
+							<i class="fa-xl fa-brands fa-reddit"></i>
 						</a>
 						<a href="">
-							<i class="fa-lg fa-brands fa-github"></i>
+							<i class="fa-xl fa-brands fa-github"></i>
 						</a>
 						<a href="">
-							<i class="fa-lg fa-brands fa-youtube"></i>
+							<i class="fa-xl fa-brands fa-youtube"></i>
 						</a>
 					</div>
 					<h6 className="pt-5">&#169; 2023 Graux Foundation. All rights reserved.</h6>
 				</div>
-				<div className="col">Fuck Me</div>
 			</div>
 		</footer>
 	);
